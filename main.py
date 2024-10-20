48bef8d5952b03f801d4989f8e63bf18ea2f6182
seoeun9 <seoeun9@naver.com>
2024-10-20 20:57:13 +0900

    2022310421 week 7 homework

---
+++ main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    hashmap = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in hashmap:
+            return [hashmap[complement], i]
+        hashmap[num] = i
+
